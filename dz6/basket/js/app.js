94e5cc936200ef1777500760ff6b477a1a575124
ia <lis42000@mail.ru>
2019-10-21 15:47:09 +0300

    Готово дз 6, сделано всё, кроме кнопки удаления из корзины

--- dz6/basket/js/app.js
+++ dz6/basket/js/app.js
@@ -11,6 +11,7 @@ let basket = {
     addItem(item) {
         if (this.items[item.id] === undefined) {
             this.items[item.id] = {
+                id: item.id,
                 name: item.name,
                 price: item.price,
                 count: 1,
@@ -18,15 +19,50 @@ let basket = {
         } else {
             this.items[item.id].count++;
         }
-    }
+    },
+
+    countPrice() {
+        let totalCost = 0;
+        for (let i in this.items) {
+            totalCost += this.items[i].price * this.items[i].count;
+        }
+        return totalCost;
+    },
+
+    renderBasket() {
+        let basketItems = `
+            <tr>
+                <td>Артикул</td>
+                <td>Товар</td>
+                <td>Количество</td>
+                <td>Сумма</td>
+                <td>Удалить</td>
+            </tr>`;
+        for (let i in this.items) {
+            basketItems += `
+                <tr>
+                    <td>${this.items[i].id}</td>
+                    <td>${this.items[i].name}</td>
+                    <td>${this.items[i].count}</td>
+                    <td>${this.items[i].count * this.items[i].price}</td>
+                    <td><i class="fa fa-ban removeButton" id="rb${i}" aria-hidden="true"></i></td>
+                </tr>
+            `;
+        }
+        basketItems += `<tr>Итого: ${this.countPrice()}</tr>`;
+        let basketContent = document.querySelector('.basketContent');
+        basketContent.innerHTML = basketItems;
+    },
 };
 
-const buyButtons = document.querySelectorAll("buyButton");
+const buyButtons = document.querySelectorAll(".buyButton");
 buyButtons.forEach(function (item) {
     item.addEventListener('click', function(event) {
         let id = event.srcElement.dataset.id;
         let name = event.srcElement.dataset.name;
         let price = event.srcElement.dataset.price;
-        basket.addItem({id: id, name: name, price: price})
+        basket.addItem({id: id, name: name, price: price});
+        basket.renderBasket();
     });
 });
+
