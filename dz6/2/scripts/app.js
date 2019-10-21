761c0b558460bb3e59ac98c83ef7eef8536fe1f5
iasasl <lis75000@bk.ru>
2019-10-21 07:49:26 +0300

    Начат слайдер

---
+++ dz6/2/scripts/app.js
@@ -0,0 +1,20 @@
+document.head.insertAdjacentHTML("afterbegin", '<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">');
+
+let slider = document.querySelector('.slider');
+
+let loadIcon = document.createElement('i');
+loadIcon.classList.add('fas', 'fa-spinner', 'fa-spin');
+slider.insertAdjacentElement("afterbegin", loadIcon);
+
+let leftArrow = document.createElement('i');
+leftArrow.classList.add('fas', 'fa-chevron-circle-left', 'slider-leftArrow');
+slider.insertAdjacentElement("beforeend", leftArrow);
+
+let rightArrow = document.createElement('i');
+rightArrow.classList.add('fas', 'fa-chevron-circle-right', 'slider-rightArrow');
+slider.insertAdjacentElement("beforeend", rightArrow);
+
+
+window.addEventListener('load', function() {
+    loadIcon.style.display = 'none';
+});
